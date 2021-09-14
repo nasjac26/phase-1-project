771705f585d02648a92a37a63c702d8651510364
Jesse <jdnely@gmail.com>
2021-09-14 10:10:25 -0700

    Added render function and image styling

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+let init = () =>
+
+{
 const searchForm = document.getElementById('search-form')
 const searchBox = document.getElementById('search')
 const searchResults = document.getElementById('search-results')
+
 
 let fetchStreamApi = (search) => {
   return fetch(`https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=`+search, {
@@ -11,15 +15,44 @@ let fetchStreamApi = (search) => {
   }
   })
   .then(response => response.json())
-  .then(json => testFunction(json))
+  .then(json => renderSearchResults(json))
   .catch(err => {
     console.error(err);
   });
 }
 
-let testFunction = (jsonData) => {
+let renderSearchResults = (jsonData) => {
+  clearSearchResults()
   console.log(jsonData)
+  jsonData.results.map(searchResultItem => {
+    let newSearchResultName = document.createElement('li')
+    newSearchResultName.innerText = searchResultItem.name
+    let newSearchResultImage = document.createElement('img')
+    newSearchResultImage.classList.add('photo')
+    newSearchResultImage.src = searchResultItem.picture
+    searchResults.appendChild(newSearchResultName)
+    searchResults.appendChild(newSearchResultImage)
+    searchResultItem.locations.map(searchResultLocation => {
+      let newSearchLink= document.createElement('a')
+      newSearchLink.href = searchResultLocation.url
+      newSearchLink.onclick = function externalAlert () {
+        return confirm("You are being sent to an external website")
+      }
+      let newSearchIcon = document.createElement('img')
+      newSearchIcon.src = searchResultLocation.icon
+    newSearchLink.appendChild(newSearchIcon)
+    searchResults.appendChild(newSearchLink)
+
+    })
+   
+
+  })
   }
+
+let clearSearchResults = () =>{
+searchResults.innerHTML = "";
+}
+
 
 let amendSearch = (event) => {
   event.preventDefault()
@@ -30,4 +63,5 @@ let amendSearch = (event) => {
 
 
 searchForm.addEventListener('submit', amendSearch)
-
+}
+document.addEventListener('DOMContentLoaded', init())
